0c5bfd4b614656193d8c5301313e9fe45f8c2b43
Mouez Jaafoura <ensimag@ensi-libre-service-065.ensimag.fr>
2024-11-09 19:41:18 +0100

    Commit des modifications

--- src/tests/TestSimulateur.java
+++ src/tests/TestSimulateur.java
@@ -18,7 +18,7 @@ public class TestSimulateur {
             robot1.setCarte(donnes.getCarte());
             robot1.setSimulateur(simulateur);
             //Case depart = robot1.getPosition(); // Exemple de case de départ
-            Case destination = donnes.getCarte().getCase(8, 8); // Exemple de case de destination
+            Case destination = donnes.getCarte().getCase(donnes.geIncendies().get(0).getPosition().getLigne(), donnes.geIncendies().get(0).getPosition().getColonne()); // Exemple de case de destination
             // Afficher le niveau du réservoir d'eau du robot 
             System.out.println(robot1.deplacerVersCase(destination));
 
