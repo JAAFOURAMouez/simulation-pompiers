2d336799e39bbc6ee56550c127d8aa05718bf2b1
Mouez Jaafoura <ensimag@ensi-libre-service-065.ensimag.fr>
2024-11-12 16:14:43 +0100

    Commit des modifications

--- src/simulateur/Simulateur.java
+++ src/simulateur/Simulateur.java
@@ -13,208 +13,178 @@ import java.util.List;
 
 
 
-public class Simulateur implements  Simulable
-{
+public class Simulateur implements Simulable {
 
-    private  Carte carte;
+    private Carte carte;
     private List<Incendie> incendies;
     private List<Robot> robots;
 
     private List<Case> initialRobotPositions;
     private List<Double> initialRobotvitesse;
     private List<Integer> initialRobotReservoir;
-    private List<Case> initialFirePositions;
     private List<Integer> initialFireintensite;
     private List<Evenement> initialEvents;
-
 
     private long dateSimulation;
     private PriorityQueue<Evenement> evenements;
     private GUISimulator gui;  
-    
+
     public Simulateur(DonneeSimulation donnes) {
-    this.carte = donnes.getCarte();
-    this.incendies = donnes.geIncendies();
-    this.robots = donnes.getRobots();
-    this.dateSimulation = 0;
-    this.evenements = new PriorityQueue<>();
+        this.carte = donnes.getCarte();
+        this.incendies = donnes.geIncendies();
+        this.robots = donnes.getRobots();
+        this.dateSimulation = 0;
+        this.evenements = new PriorityQueue<>();
 
-    // Save initial positions of robots
-    this.initialRobotPositions = new ArrayList<>();
-    this.initialRobotReservoir = new ArrayList<>();
-    this.initialRobotvitesse = new ArrayList<>();
-    this.initialEvents=new ArrayList<>();
+        // Sauvegarde des états initiaux
+        this.initialRobotPositions = new ArrayList<>();
+        this.initialRobotReservoir = new ArrayList<>();
+        this.initialRobotvitesse = new ArrayList<>();
+        this.initialEvents = new ArrayList<>();
 
-    for (Robot robot : robots) {
-        initialRobotPositions.add(robot.getPosition());  // Save initial position as Case
-        initialRobotReservoir.add(robot.getNiveauReservoirEau());
-        initialRobotvitesse.add(robot.getVitesse());
-    }
-
-    // Save initial positions of fires
-    this.initialFirePositions = new ArrayList<>();
-    this.initialFireintensite = new ArrayList<>();
-
-    for (Incendie incendie : incendies) {
-        initialFirePositions.add(incendie.getPosition());  // Save initial position as Case
-        initialFireintensite.add(incendie.getIntensite());  // Save initial position as Case
-
-    }
-
-    
-
-
-
-    // GUI setup
-    int largeur = 800;
-    int hauteur = 600;
-    int largeurCase = largeur / carte.getNbColonnes();
-    int hauteurCase = hauteur / carte.getNbLignes();
-
-    this.gui = new GUISimulator(largeur, hauteur, Color.WHITE);
-    gui.setSimulable(this);
-    afficherSimulation(carte, largeurCase, hauteurCase);
-}
-
-
-  
-
-public void afficherSimulation(Carte carte, int largeurCase, int hauteurCase) {
-    gui.reset();
-    // Redessiner la carte
-    for (int i = 0; i < carte.getNbLignes(); i++) {
-        for (int j = 0; j < carte.getNbColonnes(); j++) {
-            int x = j * largeurCase ;
-            int y = i * hauteurCase ;
-
-            Case myCase = carte.getCase(i, j);
-            NatureTerrain nature = myCase.getNature();
-            String imagePath;
-
-            switch (nature) {
-                case EAU:
-                    imagePath = "ressources/sea.png";  
-                    break;
-                case FORET:
-                    imagePath = "ressources/forest.png";  
-                    break;
-                case ROCHE:
-                    imagePath = "ressources/rocks.png";    
-                    break;
-                case HABITAT:
-                    imagePath = "ressources/city.png"; 
-                    break;
-                default:
-                    imagePath = "ressources/grass.png"; 
-                    break;
-            }
-
-            gui.addGraphicalElement(new ImageElement(x, y, imagePath, largeurCase, hauteurCase, null));
+        for (Robot robot : robots) {
+            initialRobotPositions.add(robot.getPosition());
+            initialRobotReservoir.add(robot.getNiveauReservoirEau());
+            initialRobotvitesse.add(robot.getVitesse());
         }
-    }
 
-    // Ajouter les incendies
-    String fireImagePath = "ressources/fire.png";
-    for (Incendie incendie : incendies) {
-        if (incendie.getIntensite() > 0) {
-            int x = incendie.getPosition().getColonne() * largeurCase ;
-            int y = incendie.getPosition().getLigne() * hauteurCase ;
-            gui.addGraphicalElement(new ImageElement(x, y, fireImagePath, largeurCase , hauteurCase, null));
+        // Sauvegarder les états initiaux des incendies
+        this.initialFireintensite = new ArrayList<>();
+        for (Incendie incendie : incendies) {
+            initialFireintensite.add(incendie.getIntensite());
         }
-    }
 
-    // Ajouter les robots
-    String robotImagePath;
-    for (Robot robot : robots) {
-        switch (robot.getType()) {
-            case "Drone":
-            robotImagePath= "ressources/drone.png";
-            break;
-            case"RobotAPattes":
-            robotImagePath= "ressources/apattes.png";
-            break;
-            case"RobotAChentilles":
-            robotImagePath= "ressources/achantilles.png";
-            break;
-            default:
-            robotImagePath= "ressources/firetruck.png";
-            break;
-        }
-        int x = robot.getPosition().getColonne() * largeurCase ;
-        int y = robot.getPosition().getLigne() * hauteurCase ;
-        gui.addGraphicalElement(new ImageElement(x, y, robotImagePath, largeurCase , hauteurCase , null));
-    }
-}
-    @Override
-    public void next()
-    {
+        // Sauvegarder les événements initiaux
+        initialEvents.addAll(evenements);
 
-        incrementeDate();
+        // Configuration de l'interface graphique
         int largeur = 800;
         int hauteur = 600;
-        int largeurCase=largeur/carte.getNbColonnes();
-        int hauteurCase=hauteur/carte.getNbLignes();
-        afficherSimulation(carte,largeurCase, hauteurCase);
-    
+        int largeurCase = largeur / carte.getNbColonnes();
+        int hauteurCase = hauteur / carte.getNbLignes();
 
+        this.gui = new GUISimulator(largeur, hauteur, Color.WHITE);
+        gui.setSimulable(this);
+        afficherSimulation(carte, largeurCase, hauteurCase);
     }
-    @Override
-    public void restart() {
-        // Reset the simulation date
-        this.dateSimulation = 0;
-        
-        // Clear the main event queue and reload events from the initial list
-        this.evenements.clear();
 
-        
-        // Reset robots to their initial states
-        for (int i = 0; i < robots.size(); i++) {
-            robots.get(i).setPosition(initialRobotPositions.get(i));
-            robots.get(i).setVitesse(initialRobotvitesse.get(i));
-            robots.get(i).setReservoirEau(initialRobotReservoir.get(i));
+    public void afficherSimulation(Carte carte, int largeurCase, int hauteurCase) {
+        gui.reset();
+
+        // Redessiner la carte
+        for (int i = 0; i < carte.getNbLignes(); i++) {
+            for (int j = 0; j < carte.getNbColonnes(); j++) {
+                int x = j * largeurCase;
+                int y = i * hauteurCase;
+
+                Case myCase = carte.getCase(i, j);
+                NatureTerrain nature = myCase.getNature();
+                String imagePath;
+
+                switch (nature) {
+                    case EAU:
+                        imagePath = "ressources/sea.png";
+                        break;
+                    case FORET:
+                        imagePath = "ressources/forest.png";
+                        break;
+                    case ROCHE:
+                        imagePath = "ressources/rocks.png";
+                        break;
+                    case HABITAT:
+                        imagePath = "ressources/city.png";
+                        break;
+                    default:
+                        imagePath = "ressources/grass.png";
+                        break;
+                }
+
+                gui.addGraphicalElement(new ImageElement(x, y, imagePath, largeurCase, hauteurCase, null));
+            }
         }
-        
-        // Reset fires to their initial states
-        for (int i = 0; i < incendies.size(); i++) {
-            incendies.get(i).setPosition(initialFirePositions.get(i));
-            incendies.get(i).setIntensite(initialFireintensite.get(i));
+
+        // Ajouter les incendies
+        String fireImagePath = "ressources/fire.png";
+        for (Incendie incendie : incendies) {
+            if (incendie.getIntensite() > 0) {
+                int x = incendie.getPosition().getColonne() * largeurCase;
+                int y = incendie.getPosition().getLigne() * hauteurCase;
+                gui.addGraphicalElement(new ImageElement(x, y, fireImagePath, largeurCase, hauteurCase, null));
+            }
         }
-    
-        // Redraw the GUI to reflect the initial state
+
+        // Ajouter les robots
+        for (Robot robot : robots) {
+            String robotImagePath = switch (robot.getType()) {
+                case "Drone" -> "ressources/drone.png";
+                case "RobotAPattes" -> "ressources/apattes.png";
+                case "RobotAChenilles" -> "ressources/achantilles.png";
+                default -> "ressources/firetruck.png";
+            };
+            int x = robot.getPosition().getColonne() * largeurCase;
+            int y = robot.getPosition().getLigne() * hauteurCase;
+            gui.addGraphicalElement(new ImageElement(x, y, robotImagePath, largeurCase, hauteurCase, null));
+        }
+    }
+
+    @Override
+    public void next() {
+        incrementeDate();
         int largeur = 800;
         int hauteur = 600;
         int largeurCase = largeur / carte.getNbColonnes();
         int hauteurCase = hauteur / carte.getNbLignes();
         afficherSimulation(carte, largeurCase, hauteurCase);
-        
-        // Repaint the GUI to ensure the reset state is shown
+    }
+
+    @Override
+    public void restart() {
+        // Réinitialiser la date de simulation
+        this.dateSimulation = 0;
+
+        // Réinitialiser la file d'événements
+        this.evenements.clear();
+        this.evenements.addAll(initialEvents);
+
+        // Réinitialiser les robots
+        for (int i = 0; i < robots.size(); i++) {
+            robots.get(i).setPosition(initialRobotPositions.get(i));
+            robots.get(i).setVitesse(initialRobotvitesse.get(i));
+            robots.get(i).setReservoirEau(initialRobotReservoir.get(i));
+        }
+
+        // Réinitialiser les incendies
+        for (int i = 0; i < incendies.size(); i++) {
+            incendies.get(i).setIntensite(initialFireintensite.get(i));
+        }
+
+        // Redessiner la simulation
+        int largeur = 800;
+        int hauteur = 600;
+        int largeurCase = largeur / carte.getNbColonnes();
+        int hauteurCase = hauteur / carte.getNbLignes();
+        afficherSimulation(carte, largeurCase, hauteurCase);
         gui.repaint();
     }
 
-
-    public void ajouteEvenement(Evenement e)
-    {
+    public void ajouteEvenement(Evenement e) {
         evenements.add(e);
+        initialEvents.add(e);
     }
 
-    private  void incrementeDate() {
-        
+    private void incrementeDate() {
         while (!evenements.isEmpty() && evenements.peek().getDate() <= dateSimulation) {
             Evenement e = evenements.poll();
             e.execute();
         }
-
         dateSimulation++;
     }
 
-    public  long getDateSimulation()
-    {
+    public long getDateSimulation() {
         return dateSimulation;
     }
 
-    private boolean simulationTerminee(long date)
-    {
-        return (date > dateSimulation);
+    private boolean simulationTerminee(long date) {
+        return date > dateSimulation;
     }
-
 }
